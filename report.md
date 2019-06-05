7ee9071ded5cdb30f06738bf2f5904ea17ce3595
yth98 <b06901031@ntu.edu.tw>
2019-06-05 13:02:50 +0800

    first commit

---
+++ report.md
@@ -0,0 +1,30 @@
+# 2019 OS Project 2 - Synchronous Virtual Device
+> http://rswiki.csie.org/dokuwiki/courses:107_2:project_2
+
+## 設計 Program Design
+
+### How to Run
+
+    sudo ./compile.sh
+    sudo -i
+    cd `...`
+    ./run.sh
+
+    logout
+    sudo ./clean.sh
+
+## 執行結果 The Result
+
+## 效能比較 Performance Comparison
+
+## 組員貢獻 Contributions
+
+- B04201020：
+
+- B04201021：
+
+- B04201043：
+
+- B05902028：
+
+- B06901031：
---
+++ run.sh
@@ -0,0 +1,43 @@
+<<@
+Usage:
+sudo -i
+./run.sh
+@
+# Reference: Bash script processing limited number of commands in parallel
+# https://stackoverflow.com/posts/19543185/revisions
+
+# use > instead of >> here to truncate the log
+user_program/master data/file1_in fcntl > log_master &
+user_program/slave data/file1_out fcntl 127.0.0.1 > log_slave &
+wait
+echo Diff of file1 on fcntl/fcntl:
+diff data/file1_in data/file1_out
+wait
+
+user_program/master data/file2_in fcntl >> log_master &
+user_program/slave data/file2_out fcntl 127.0.0.1 >> log_slave &
+wait
+echo Diff of file2 on fcntl/fcntl:
+diff data/file2_in data/file2_out
+wait
+
+user_program/master data/file3_in fcntl >> log_master &
+user_program/slave data/file3_out fcntl 127.0.0.1 >> log_slave &
+wait
+echo Diff of file3 on fcntl/fcntl:
+diff data/file3_in data/file3_out
+wait
+
+user_program/master data/file4_in fcntl >> log_master &
+user_program/slave data/file4_out fcntl 127.0.0.1 >> log_slave &
+wait
+echo Diff of file4 on fcntl/fcntl:
+diff data/file4_in data/file4_out
+wait
+
+echo
+echo Log of Master:
+cat log_master
+echo
+echo Log of Slave:
+cat log_slave
